a814dc9e5633fb5c83b31f7100e68609a2629d97
toranit2541 <toranit.won.41@gmail.com>
2025-03-05 17:34:27 +0700

    hot

--- rchapi/wsgi.py
+++ rchapi/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+# Add your project directory to the sys.path
+sys.path.append('/home/toranit/rchapi-main')
+
+# Activate the virtual environment
+activate_this = '/home/toranit/rchapi-main/venv/bin/activate_this.py'
+exec(open(activate_this).read(), dict(__file__=activate_this))
+
+# Set the Django settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'rchapi-main.settings'
+
+# Import Django WSGI application
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rchapi.settings')
-
 application = get_wsgi_application()
+
