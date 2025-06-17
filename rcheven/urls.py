daaaea3a0b989dae6707483b392e87c7b3f012ea
toranit41 <toranit.won.41@gmail.com>
2025-06-17 08:44:54 +0700

    send edit

--- rcheven/urls.py
+++ rcheven/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EventViewSet, TestRchEven
+from .views import EventQueryView, EventViewSet, TestRchEven
 
 router = DefaultRouter()
 router.register(r'event', EventViewSet)
 
 urlpatterns = [
     path('test/', TestRchEven.as_view(), name='test_event'),
-    path('event/', EventQueryView.as_view(), name='event')
+    path('event/', EventQueryView.as_view(), name='event'),
     path('', include(router.urls)),
 ]
